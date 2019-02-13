cf3b45f6d7f139f8d5c21fa2baeef225e06d82e9
Mindaugas Kestenis <mkestenis@gmail.com>
2019-02-13 09:16:12 +0200

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Calculator {
+    private List<Double> numbers;
+
+    public Calculator() {
+        this.numbers = new ArrayList<>();
+    }
+
+    public boolean isPositive(double number){
+        return number >= 0;
+    }
+
+    public boolean notZero(double number){
+        return number != 0;
+    }
+}
